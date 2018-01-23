00979e6089b1bd1aa9c710f69fd9226d53d4f46b
k.siu92@gmail.com <k.siu92@gmail.com>
2018-01-23 17:27:04 -0500

    Made asteroids rotate, but hit collision slightly off now

--- bundle.js
+++ bundle.js
@@ -222,7 +222,6 @@ $(window).ready(function(){
 
   const sound = document.getElementById("sound-btn");
   sound.addEventListener("mousedown", function() {    // clicking music btn to mute
-    const sound = document.getElementById("sound-btn");
     if (game.gameState == "GAME_SCREEN") {
       if (!game.backgroundMusic.paused) {
         $(sound).css('opacity', '0.4');
@@ -263,7 +262,6 @@ class Game {
     this.food = [];
     this.obstacles = [];
     this.score = new Score(1);
-    // this.muteSounds();
 
     this.gameState = "MENU_SCREEN";
     this.volumeState = "ON";
@@ -466,6 +464,7 @@ class Food {
   constructor() {
     this.sushi = new Image();
     this.drumstick = new Image();
+    this.ang = 0;
 
     this.foodHeight = 0;
 
@@ -486,6 +485,7 @@ class Food {
       this.foodHeight = 50;
       this.foodWidth = 30;
     }
+
   }
 
   update() {
@@ -525,6 +525,7 @@ class Obstacles {
     this.asteroid_loc_y = Math.floor(Math.random() * (270-10) + 10);
     this.asteroidHeight = 35;
     this.asteroidWidth = 50;
+    this.ang = 0;
   }
 
   update() {
@@ -532,7 +533,12 @@ class Obstacles {
   }
 
   draw(ctx) {
-    ctx.drawImage(this.asteroid, this.asteroid_loc_x, this.asteroid_loc_y);
+    ctx.save();
+    ctx.translate(this.asteroid_loc_x, this.asteroid_loc_y);
+    ctx.rotate(Math.PI / 180 * (this.ang += .2));
+    ctx.drawImage(this.asteroid, 0, 0);
+    // ctx.drawImage(this.asteroid, this.asteroid_loc_x, this.asteroid_loc_y);
+    ctx.restore();
   }
 
   asteroidInBound() {
@@ -543,6 +549,14 @@ class Obstacles {
     }
   }
 }
+
+// function drawGuy() {
+//     ctx.save();
+//     ctx.translate(x,y);
+//     ctx.rotate(angle * Math.PI / 180);
+//     ctx.drawImage(guy, 0, 0);
+//     ctx.restore();
+// }
 
 module.exports = Obstacles;
 
